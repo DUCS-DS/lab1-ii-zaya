8df36183c583cf1ba8bab8761606335592f95f45
ii-zaya <isaiahblittle@gmail.com>
2025-01-24 12:15:10 -0600

    updated

--- edit_me.py
+++ edit_me.py
@@ -4,11 +4,11 @@ def find_max(lst):
 
     current_max = lst[0]
 
-    print('asdfasdf')
-    #
-    #  YOUR CODE GOES HERE
-    #
+    for i in range(1, len(lst)):
+        if lst[i] > current_max:
+            current_max = lst[i] 
 
+    
     return current_max
 
 test_list = [2112*i % 2024 for i in range(10000)]
